34f50b5d07eebeba182aba552662e6c49daf304e
s12171934 <ssong99999@naver.com>
2023-10-25 15:03:13 +0900

    sixth commit - Change file name

--- src/week2/Calc.java
+++ src/week2/Calc.java
@@ -2,7 +2,7 @@ package week2;
 
 import java.util.Scanner;
 
-public class Wed {
+public class Calc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(true){
