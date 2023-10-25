546318a902bf57e14215f660e1a491206faa7854
s12171934 <ssong99999@naver.com>
2023-10-25 11:19:43 +0900

    second commit - Change Input form

--- src/week2/Wed.java
+++ src/week2/Wed.java
@@ -6,7 +6,9 @@ public class Wed {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(true){
-            System.out.print("1. 더하기 \n2. 빼기 \n3. 곱하기 \n4. 나누기 \n5. 나머지 \nq. 종료 \n>>");
+            System.out.println("##################################");
+            System.out.println("1. 더하기 \n2. 빼기 \n3. 곱하기 \n4. 나누기 \n5. 나머지 \nq. 종료");
+            System.out.print("##################################\n>> ");
             String op = sc.nextLine();
             int a = 0;
             int b = 0;
