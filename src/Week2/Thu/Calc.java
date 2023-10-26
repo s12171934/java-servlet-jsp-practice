4971f54565bdc750edebbf7e3118a1ec8bdd8856
s12171934 <ssong99999@naver.com>
2023-10-26 11:31:40 +0900

    23.10.26. 11:31 AM

--- src/Week2/Thu/Calc.java
+++ src/Week2/Thu/Calc.java
@@ -1,4 +1,38 @@
 package Week2.Thu;
 
 public class Calc {
+    private static final double PI = 3.14159;
+    private int result;
+
+    public static double getPI(){
+        return PI;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public void plus(int a, int b){
+        this.result = a + b;
+    }
+
+    public void minus(int a, int b){
+        this.result = a - b;
+    }
+
+    public void multiply(int a, int b){
+        this.result = a * b;
+    }
+
+    public void divide(int a, int b){
+        this.result = a / b;
+    }
+
+    public void modular(int a, int b){
+        this.result = a % b;
+    }
 }
--- src/Week2/Thu/CalcExecute.java
+++ src/Week2/Thu/CalcExecute.java
@@ -1,4 +1,13 @@
 package Week2.Thu;
 
 public class CalcExecute {
+    public static void main(String[] args) {
+        int result;
+        Calc calc = new Calc();
+        calc.plus(3, 5);
+        result = calc.getResult();
+        System.out.println(result);
+
+        System.out.println(Calc.getPI());
+    }
 }
