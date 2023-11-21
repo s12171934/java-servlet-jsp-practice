b642cb2ede1f1146bcf6014b46deadcc40eab2a5
s12171934 <ssong99999@naver.com>
2023-11-21 17:46:08 +0900

    jsp 생성/ book user 생성

---
+++ src/main/java/bookManager/BookManager.java
@@ -0,0 +1,38 @@
+package bookManager;
+
+public class BookManager implements BM{
+    @Override
+    public void addBook() {
+
+    }
+
+    @Override
+    public void sortBook() {
+
+    }
+
+    @Override
+    public void searchBook() {
+
+    }
+
+    @Override
+    public void editBook() {
+
+    }
+
+    @Override
+    public void removeBook() {
+
+    }
+
+    @Override
+    public void checkOutBook() {
+
+    }
+
+    @Override
+    public void checkInBook() {
+
+    }
+}
---
+++ src/main/java/userManager/UserManager.java
@@ -0,0 +1,28 @@
+package userManager;
+
+public class UserManager implements UM{
+    @Override
+    public void signUp() {
+
+    }
+
+    @Override
+    public void sortUser() {
+
+    }
+
+    @Override
+    public void searchUser() {
+
+    }
+
+    @Override
+    public void editUser() {
+
+    }
+
+    @Override
+    public void removeUser() {
+
+    }
+}
