9c8e658ca6948d7037ec286861735d2c9850db2c
s12171934 <ssong99999@naver.com>
2024-01-09 15:42:38 +0900

    jdbc 구현완료

--- src/main/java/com/kitri/myservletboard/dao/BoardJdbc.java
+++ src/main/java/com/kitri/myservletboard/dao/BoardJdbc.java
@@ -2,10 +2,7 @@ package com.kitri.myservletboard.dao;
 
 import com.kitri.myservletboard.data.Board;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -44,8 +41,12 @@ public class BoardJdbc implements BoardDao{
                 Board board = new Board(id,title,content,writer,createAt,viewCount,commentCount);
                 boards.add(board);
             }
-            return boards;
 
+            rs.close();
+            stmt.close();
+            conn().close();
+
+            return boards;
         } catch (Exception e){
             return null;
         }
@@ -53,21 +54,86 @@ public class BoardJdbc implements BoardDao{
 
     @Override
     public Board getById(Long id) {
-        return null;
+        try {
+            String sql = "SELECT * FROM board WHERE id = ?";
+            PreparedStatement pstmt = conn().prepareStatement(sql);
+            pstmt.setLong(1,id);
+            ResultSet rs = pstmt.executeQuery();
+            Board board = null;
+            while (rs.next()){
+                String title = rs.getString("title");
+                String writer = rs.getString("writer");
+                String content = rs.getString("content");
+                LocalDateTime createAt = rs.getTimestamp("createAt").toLocalDateTime();
+                int viewCount = rs.getInt("viewCount");
+                int commentCount = rs.getInt("commentCount");
+                board = new Board(id,title,content,writer,createAt,viewCount,commentCount);
+            }
+
+            rs.close();
+            pstmt.close();
+            conn().close();
+
+            return board;
+        } catch (Exception e){
+            return null;
+        }
     }
 
     @Override
     public void save(Board board) {
+        try {
+            String title = board.getTitle();
+            String writer = board.getWriter();
+            String content = board.getContent();
+            String sql = "INSERT INTO board (title,writer,content) VALUES (?,?,?)";
+            PreparedStatement pstmt = conn().prepareStatement(sql);
+            pstmt.setString(1,title);
+            pstmt.setString(2,writer);
+            pstmt.setString(3,content);
+            pstmt.executeUpdate();
+            conn().commit();
 
+            pstmt.close();
+            conn().close();
+
+        } catch (Exception e){}
     }
 
     @Override
     public void update(Board board) {
+        try {
+            Long id = board.getId();
+            String title = board.getTitle();
+            String content = board.getContent();
+            String sql = "UPDATE board SET title = ?, content = ? WHERE id = ?";
+            PreparedStatement pstmt = conn().prepareStatement(sql);
+            pstmt.setString(1,title);
+            pstmt.setString(2,content);
+            pstmt.setLong(3,id);
+            pstmt.executeUpdate();
+            conn().commit();
+
+            pstmt.close();
+            conn().close();
+
+        } catch (Exception e){}
 
     }
 
     @Override
     public void delete(Board board) {
+        try {
+            Long id = board.getId();
+            String sql = "DELETE FROM board WHERE id = ?";
+            PreparedStatement pstmt = conn().prepareStatement(sql);
+            pstmt.setLong(1,id);
+            pstmt.executeUpdate();
+            conn().commit();
 
+            pstmt.close();
+            conn().close();
+
+        } catch (Exception e){}
     }
 }
