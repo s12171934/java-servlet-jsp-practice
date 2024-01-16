041cd6eb5c6f8cbf09502e6fab3d7109ed4bb0b6
s12171934 <ssong99999@naver.com>
2024-01-16 09:21:14 +0900

    간단한 JDBC

--- src/main/java/com/kitri/myservletboard/dao/MemberJdbc.java
+++ src/main/java/com/kitri/myservletboard/dao/MemberJdbc.java
@@ -1,4 +1,22 @@
 package com.kitri.myservletboard.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class MemberJdbc {
+    private MemberJdbc(){}
+    private static MemberJdbc instance = new MemberJdbc();
+    public static MemberJdbc getInstance(){return instance;}
+    private Connection conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String jdbcurl = "jdbc:mysql://localhost:3306/my_servlet_board";
+            String userId = "root";
+            String userPass = "1234";
+            Connection conn = DriverManager.getConnection(jdbcurl,userId,userPass);
+            return conn;
+        } catch (Exception e){
+            return null;
+        }
+    }
 }
